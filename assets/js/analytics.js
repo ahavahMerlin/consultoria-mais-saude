0d75d771568f46f77f0f38e59b14a2d13b103654
DeBocaEmBoca <vendamais@gmail.com>
2026-02-09 14:16:30 -0300

    Add Google Analytics 4 tracking code
    Added Google Analytics 4 tracking code to analytics.js

---
+++ assets/js/analytics.js
@@ -0,0 +1,7 @@
+// Google Analytics 4
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+// SUBSTITUA PELO SEU ID GA4
+gtag('config', 'G-XXXXXXXXXX');
